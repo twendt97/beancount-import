f5c4cb5937cfcee421371fb231627f1d97c1d2e0
Thilo Wendt <mail@thilo-wendt.de>
2024-01-03 23:55:48 +0100

    Add account mapper to master card importer

--- rubti_beancount_import/sparkasse/master_card/master_card.py
+++ rubti_beancount_import/sparkasse/master_card/master_card.py
@@ -1,8 +1,5 @@
 import csv
-import json
-from collections.abc import Sequence
 from datetime import datetime
-from decimal import Decimal
 from pathlib import Path
 
 from beancount.core import amount, data
@@ -34,31 +31,22 @@ class SpkMasterCardImporter(ImporterProtocol):
     last_four_digits: str
     account: str
     currency: str
-    date_format: str
-    file_encoding: str
-    _fields: Sequence[str]
-    _txn_infos: dict = {}
-    account_mapper: utils.AccountMapper = None
+    _date_format = "%d.%m.%y"
+    _file_encoding = "ISO-8859-1"
+    _acc_map: utils.AccountMapper = None
 
     def __init__(
         self,
         account: str,
         last_four_digits: str,
-        account_mapping: Path = None,
+        account_mapping: str = None,
         currency: str = "EUR",
-        date_format: str = "%d.%m.%y",
-        file_encoding: str = "ISO-8859-1",
     ) -> None:
         self.account = account
         self.last_four_digits = last_four_digits
         self.currency = currency
-        self.date_format = date_format
-        self.file_encoding = file_encoding
         self._fields = DEFAULT_FIELDS
-        if account_mapping is not None:
-            f = open(account_mapping)
-            self._txn_infos = json.load(f)
-            f.close()
+        self._acc_map = utils.AccountMapper(account_mapping)
 
     def name(self) -> str:
         return "Sparkasse MasterCard"
@@ -74,7 +62,7 @@ class SpkMasterCardImporter(ImporterProtocol):
         if Path(file.name).suffix.lower() != ".csv":
             return False
 
-        with open(file.name, encoding=self.file_encoding) as f:
+        with open(file.name, encoding=self._file_encoding) as f:
             header = f.readline().strip()
             csv_row = f.readline().strip()
 
@@ -90,59 +78,38 @@ class SpkMasterCardImporter(ImporterProtocol):
         return self.account
 
     def extract(self, file, existing_entries=None):
-        entries = []
+        if existing_entries:
+            entries = existing_entries
+        else:
+            entries = []
         index = 0
-        with open(file.name, encoding=self.file_encoding) as f:
+        with open(file.name, encoding=self._file_encoding) as f:
             for index, row in enumerate(
                 csv.DictReader(f, delimiter=";", quotechar='"')
             ):
                 meta = data.new_metadata(filename=file.name, lineno=index)
                 date: datetime = datetime.strptime(
-                    row["Buchungsdatum"], self.date_format
+                    row["Buchungsdatum"], self._date_format
                 ).date()
                 payee = row["Transaktionsbeschreibung"]
                 units = amount.Amount(
                     utils.format_amount(row["Buchungsbetrag"]), currency=self.currency
                 )
-                postings = [
-                    data.Posting(
-                        self.account,
-                        units=units,
-                        cost=None,
-                        price=None,
-                        flag=None,
-                        meta=None,
-                    )
-                ]
-                txn_payee = payee
-                narration = ""
-                if payee in self._txn_infos:
-                    postings.append(
-                        data.Posting(
-                            account=self._txn_infos[payee]["account"],
-                            units=-units,
-                            price=None,
-                            flag=None,
-                            meta=None,
-                            cost=None,
-                        )
-                    )
-                    if "payee" in self._txn_infos[payee]:
-                        txn_payee = self._txn_infos[payee]["payee"]
-                    if "narration" in self._txn_infos[payee]:
-                        narration = self._txn_infos[payee]["narration"]
+                postings = [utils.create_posting(self.account, units, meta)]
 
-                txn = data.Transaction(
-                    meta=meta,
-                    date=date,
-                    flag=self.FLAG,
-                    payee=txn_payee,
-                    narration=narration,
-                    tags=data.EMPTY_SET,
-                    links=data.EMPTY_SET,
-                    postings=postings,
+                if self._acc_map.account(payee) is not None:
+                    postings.append(
+                        utils.create_posting(self._acc_map.account(payee), -units, None)
+                    )
+                entries.append(
+                    utils.create_transaction(
+                        postings,
+                        date,
+                        meta,
+                        self._acc_map.new_payee(payee),
+                        self._acc_map.narration(payee),
+                    )
                 )
-                entries.append(txn)
         return entries
 
     def file_name(self, file):
--- rubti_beancount_import/utils.py
+++ rubti_beancount_import/utils.py
@@ -4,17 +4,9 @@ from identify import identify
 import json
 import yaml
 from dataclasses import dataclass
-from beancount.core import amount, data
-from beancount.core import flags
 
-
-@dataclass
-class PayeeInfo:
-    """User supplied information for specific payees"""
-
-    account: str = None
-    new_payee: str = None
-    narration: str = None
+from beancount.core import amount, data, flags
+from datetime import datetime
 
 
 def format_amount(amount: str) -> Decimal:
@@ -22,37 +14,71 @@ def format_amount(amount: str) -> Decimal:
     return Decimal(amount.replace(".", "").replace(",", "."))
 
 
-class AccountMapper:
-    mappings = {}
+def create_posting(account: str, units: amount, meta: dict) -> data.Posting:
+    return data.Posting(
+        account=account,
+        units=units,
+        cost=None,
+        price=None,
+        flag=None,
+        meta=meta,
+    )
 
-    def __init__(self, mapping_file: str):
+
+def create_transaction(
+    postings: list[data.Posting], date: datetime, meta: dict, payee: str, narration: str
+) -> data.Transaction:
+    return data.Transaction(
+        meta=meta,
+        date=date,
+        flag=flags.FLAG_OKAY,
+        payee=payee,
+        narration=narration,
+        tags=data.EMPTY_SET,
+        links=data.EMPTY_SET,
+        postings=postings,
+    )
+
+
+class AccountMapper:
+    _mappings = {}
+
+    def __init__(self, mapping_file: str = None):
+        if mapping_file is None:
+            return
         file_path = Path(mapping_file)
         if not file_path.is_file():
             raise FileNotFoundError(f"File {mapping_file} does not exist")
         file_tags = identify.tags_from_path(mapping_file)
         with open(file_path) as f:
             if "yaml" in file_tags:
-                self.mappings = yaml.safe_load(f)
+                self._mappings = yaml.safe_load(f)
             elif "json" in file_tags:
-                self.mappings = json.load(f)
+                self._mappings = json.load(f)
             else:
                 raise ValueError(
                     f"Format of file {mapping_file} has not been recognized. Make sure it is YAML or JSON"
                 )
 
-    def get_payee_details(self, payee: str):
-        payee_info = PayeeInfo()
+    def new_payee(self, payee: str) -> str:
+        if not self._payee_known(payee) or "payee" not in self._mappings[payee]:
+            return payee
+        else:
+            return self._mappings[payee]["payee"]
+
+    def account(self, payee: str) -> str | None:
         if self._payee_known(payee):
-            payee_info.account = self.mappings[payee]["account"]
-            if "payee" in self.mappings[payee]:
-                payee_info.new_payee = self.mappings[payee]["payee"]
-            if "narration" in self.mappings[payee]:
-                payee_info.narration = self.mappings[payee]["narration"]
-            return payee_info
+            return self._mappings[payee]["account"]
         else:
             return None
 
+    def narration(self, payee: str) -> str:
+        if not self._payee_known(payee) or "narration" not in self._mappings[payee]:
+            return ""
+        else:
+            return self._mappings[payee]["narration"]
+
     def _payee_known(self, payee: str) -> bool:
-        if payee in self.mappings:
+        if payee in self._mappings:
             return True
         return False
